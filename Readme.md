9f1c3ea39ccf4a9a88675b1daadfe20acaddaab5
Clement1987 <jaminclement1987@gmail.com>
2020-01-13 11:16:35 +0100

    exercice done

---
+++ Readme.md
@@ -0,0 +1,7 @@
+L'objectif de cet exercice est de :
+
+Trier une liste d'objets avec filter
+
+Utiliser l'interface fonctionnelle Predicate
+
+Comprendre et utiliser les lambdas
--- Shield.java
+++ Shield.java
@@ -4,33 +4,38 @@ import java.util.stream.Collectors;
 
 public class Shield {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        List<Hero> heroes = new ArrayList<>();
-        heroes.add(new Hero("Black Widow", 34, false));
-        heroes.add(new Hero("Captain America", 100, false));
-        heroes.add(new Hero("Vision", 3, true));
-        heroes.add(new Hero("Iron Man", 48, false));
-        heroes.add(new Hero("Scarlet Witch", 29, true));
-        heroes.add(new Hero("Thor", 1501, false));
-        heroes.add(new Hero("Spider-Man", 18, false));
-        heroes.add(new Hero("Hulk", 49, true));
-        heroes.add(new Hero("Doctor Strange", 42, false));
+		List<Hero> heroes = new ArrayList<>();
+		heroes.add(new Hero("Black Widow", 34, false));
+		heroes.add(new Hero("Captain America", 100, false));
+		heroes.add(new Hero("Vision", 3, true));
+		heroes.add(new Hero("Iron Man", 48, false));
+		heroes.add(new Hero("Scarlet Witch", 29, true));
+		heroes.add(new Hero("Thor", 1501, false));
+		heroes.add(new Hero("Spider-Man", 18, false));
+		heroes.add(new Hero("Hulk", 49, true));
+		heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
-        // TODO 1 : filter heroes in order to found heroes older than 59
+		List<Hero> elders = heroes.stream()
+				.filter(elder -> elder.getAge() > 59)
+				.collect(Collectors.toList());
+		
+		// TODO 1 : filter heroes in order to found heroes older than 59
+		System.out.println("\nElders:");
+		for (Hero elder : elders) {
+			System.out.println(elder.getName());
+		}
 
-        System.out.println("\nElders:");
-        for (Hero elder : elders) {
-            System.out.println(elder.getName());
-        }
+		List<Hero> intolerants = heroes.stream()
+				.filter(intolerant -> intolerant.isGlutenIntolerant() == true)
+				.collect(Collectors.toList());
+		
+		// TODO 2 : filter heroes in order to found heroes that are gluten intolerants
 
-        List<Hero> intolerants;
-        // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
-
-        System.out.println("\nGluten intolerants:");
-        for (Hero intolerant : intolerants) {
-            System.out.println(intolerant.getName());
-        }
-    }
+		System.out.println("\nGluten intolerants:");
+		for (Hero intolerant : intolerants) {
+			System.out.println(intolerant.getName());
+		}
+	}
 }
